6d605b11286814691128a408d7deea6b40bce3d2
ミューズ / Muse <91707462+MuseMyuzu@users.noreply.github.com>
2022-06-30 20:24:09 +0900

    load from Resources in Score.cs

--- Assets/ScriptsOfficial/Score.cs
+++ Assets/ScriptsOfficial/Score.cs
@@ -9,10 +9,11 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
-    public Sprite[] fontTemplateList = new Sprite[10];
-    public GameObject scoreObject;
+    //0～9までの画像
+    private Sprite[] fontTemplateList;
+    private GameObject scoreObjects;
     private SpriteRenderer spRenderer;
-    public int score;
+    private int score;
     private static int[] rensaBonus = new int[] { 0, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640, 672 };
     private static int[] pieceBonus = new int[] { 0, 0, 0, 0, 2, 3, 4, 5, 6, 7, 10, 10 };
     private static int[] colorBonus = new int[] { 0, 0, 3, 6, 12, 24 };
@@ -20,7 +21,6 @@ public class Score : MonoBehaviour
     public void initialize()
     {
         score = 0;
-        this.showScore();
     }
 
     public void showScore()
@@ -32,7 +32,7 @@ public class Score : MonoBehaviour
             // 10で割ったあまりを求めて、一番下の桁を取り出す
             int number = scoreTemp % 10;
 
-            GameObject child = scoreObject.transform.GetChild(i).gameObject;
+            GameObject child = scoreObjects.transform.GetChild(i).gameObject;
             spRenderer = child.gameObject.GetComponent<SpriteRenderer>();
             spRenderer.sprite = fontTemplateList[number];
 
@@ -61,17 +61,22 @@ public class Score : MonoBehaviour
         }
 
         this.addScore(scale * piece * 10);
-        this.showScore();
         Debug.Log("rensa=" + rensa + " piece=" + piece + " color=" + color + " score=" + score);
     }
 
     public void addScore(int addScore)
     {
         score += addScore;
+        //スコアが変わったら、スコアを再表示
+        this.showScore();
     }
 
     void Start()
     {
-
+        //数字の画像を読み込み
+        this.fontTemplateList = Resources.LoadAll<Sprite>("img/number/");
+        //スコアを表示するオブジェクト
+        this.scoreObjects = GameObject.Find("ScoreObjects");
+        Debug.Log("fontTemplateList = " + fontTemplateList);
     }
 }
